188c869457fae603b65e0010388d240654f7c64e
FubukiGO <wzysz555@qq.com>
2020-01-13 15:45:02 +0800

    update

--- src/main/java/com/akhan/tv/TableResearch.java
+++ src/main/java/com/akhan/tv/TableResearch.java
@@ -127,7 +127,7 @@ public class TableResearch extends JFrame {
 
         getContentPane().add(p1, "North");
 
-        getContentPane().add(new JLabel("<html><font> 版本: V0.9    日期：2020 </font> <font color=blue>EMail:wzysz555@foxmail.com</font></html>"), "South");
+        getContentPane().add(new JLabel("<html><font> 版本: V1.1    日期：2020-01 </font> <font color=blue>&nbsp;&nbsp;Created By akhan</font></html>"), "South");
 
         sp1 = new JScrollPane(lsTable);
 
